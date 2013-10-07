dd781835a3084ae158d6b3ec0798ecedbf3222f9
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-10-07 14:21:43 +0200

    Added libgdx package

---
+++ src/de/myreality/parallax/libgdx/GdxTexture.java
@@ -0,0 +1,73 @@
+
+package de.myreality.parallax.libgdx;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import de.myreality.parallax.LayerTexture;
+import de.myreality.parallax.util.Color;
+
+public class GdxTexture implements LayerTexture {
+
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+	
+	private Texture texture;
+	
+	private SpriteBatch batch;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+	
+	public GdxTexture(Texture texture, SpriteBatch batch) {
+		this.texture = texture;
+		this.batch = batch;
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public void draw(float x, float y, float width, float height, Color filter) {
+		if (texture != null) {
+			batch.setColor(filter.r, filter.g, filter.b, filter.a);
+			batch.draw(texture, x, y, width, height);
+		}
+	}
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getWidth() {
+		return texture.getWidth();
+	}
+
+	@Override
+	public int getHeight() {
+		return texture.getHeight();
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+
+}
---
+++ src/de/myreality/parallax/libgdx/PreprocessedTexture.java
@@ -0,0 +1,60 @@
+package de.myreality.parallax.libgdx;
+
+import de.myreality.parallax.LayerTexture;
+import de.myreality.parallax.util.Color;
+
+public class PreprocessedTexture implements LayerTexture {
+
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public void draw(float x, float y, float width, float height, Color filter) {
+		
+
+	}
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+
+}
